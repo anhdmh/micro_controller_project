5aa7d530c533533886f67b22ad021570a45a1390
anhdmh <85148223+anhdmh@users.noreply.github.com>
2021-06-06 23:56:09 +0700

    Thêm comment Bluetooth

--- bluetooth_module.h
+++ bluetooth_module.h
@@ -1,60 +1,63 @@
-//******Initialize Bluetooth using USART********//
 void Init_Bluetooth()
 {
-   //Set the pins of RX and TX//
+   //Khai báo các chân RX,TX
     TRISC6=1;
     TRISC7=1;
     
-  //Set the baud rate using the look up table in datasheet(pg114)//
-    BRGH=1;      //Always use high speed baud rate with Bluetooth else it wont work
-    SPBRG  =129;
+  //chọn baud rate với tần số 20M và baudrate = 9k6 theo datasheet
+    BRGH = 1;      //Chọn baudrate ở tốc độ cao
+    SPBRG = 129;
     
-    //Turn on Asyc. Serial Port//
+    // config để Thiết lập chế độ bán đồng bộ. Serial Port//
     SYNC=0;
     SPEN=1;
     
-    //Set 8-bit reception and transmission
+    //thiết lập 8bit reception và 8bit transmission
     RX9=0;
     TX9=0;
 
-   //Enable transmission and reception//
-    TXEN=1; 
-    CREN=1; 
+   
+    TXEN=1; // TXEN = 1 gửi data lt
+    CREN=1; // CREN = 1 cho phép nhận data liên tục
     
-    //Enable global and ph. interrupts//
+    //Bật các flag này để sử dụng interupt cho TX RX
     GIE = 1;
     PEIE= 1;
   
-    //Enable interrupts for Tx. and Rx.//
+    //Config interupt cho TX và RX//
     RCIE=1;
     TXIE=1;
 }
-//___________BT initialized_____________//
-//Function to load the Bluetooth Rx. buffer with one char.//
+
+//Load đến RX của bluetooth 1 ký tự
 void BT_load_char(char byte)  
 {
+   // gán dữ liệu vào thanh ghi TXREG 
     TXREG = byte;
+   // Check status bit TRMT, và TXIF để xem đã gửi xong dữ liệu chưa
     while(!TXIF);  
     while(!TRMT);
 }
-//End of function//
-//Function to Load Bluetooth Rx. buffer with string//
+
+
+// load 1 string đến RX bluetooth - sử dụng lại hàm loadchar
 void BT_load_string(char* string)
 {
     while(*string)
     BT_load_char(*string++);
 }
-//End of function//
+
+
 
 //Function to broadcast data from RX. buffer//
 void broadcast_BT()
 {
-  TXREG = 13;  
+  TXREG = 13;  // 13 là ký tự xuống dòng, đánh dấu kết thúc dữ liệu
   __delay_ms(500);
 }
-//End of function//
 
-//Function to get a char from Rx.buffer of BT//
+
+
 char BT_get_char(void)   
 {
     if(OERR) // check for over run error 
@@ -63,12 +66,11 @@ char BT_get_char(void)
         CREN = 1; //Reset CREN
     }
     
-    if(RCIF==1) //if the user has sent a char return the char (ASCII value)
+    if(RCIF==1)
     {
-    while(!RCIF);  
-    return RCREG;
+      while(!RCIF);  //Dùng polling, liên tục check interrupt flag bit RCIF xem đã nhận data xong chưa
+      return RCREG;
     }
-    else //if user has sent no message return 0
+    else //nếu không gửi => return 0
         return 0;
 }
-//End of function/
