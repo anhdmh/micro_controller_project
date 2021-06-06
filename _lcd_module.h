5f114d1171b89c9d4542f9c38cd4723380849f45
anhdmh <85148223+anhdmh@users.noreply.github.com>
2021-06-07 02:42:23 +0700

    update comment lcd 1

--- _lcd_module.h
+++ _lcd_module.h
@@ -1,4 +1,3 @@
-//cac chan cua LCD duoc ket noi voi vi dieu khien
 #define LCD_RS RD1                                    //RS noi vao RD1
 #define LCD_EN RD0                                    //EN noi vao RD0
 #define LCD_D4 RD4                                    //D4 noi vao RD4
@@ -15,22 +14,24 @@ void LCD_Send4Bit( unsigned char a){
     LCD_D6=(a>>2)& 0x01;                               
     LCD_D7=(a>>3)& 0x01;                    
     
+    // phát 1 xung high-to-low transition của tín hiệu chân E để đưa các bit vào thanh ghi.
     LCD_EN = 1;                                         
-    __delay_us(5);// delay cho 
+    __delay_us(5);
     LCD_EN = 0;
     __delay_us(40); 
 }
 
 void LCD_SendCommand (unsigned char command ){
-    LCD_Send4Bit  (command >>4);//Gui 4 bit cao ( dich phai 4 bit)
-    LCD_Send4Bit  (command );  //Gui 4 bit thap
+    LCD_Send4Bit  (command >>4);//Gui 4 bit cao ( 4 bit đầu)
+    LCD_Send4Bit  (command );  //Gui 4 bit thap  (4 bit cuối)
 }
 
 void LCD_Init (void){
     LCD_TRIS = 0;                               
     LCD_RS = 0;
-    __delay_ms(20);
-    
+    __delay_ms(20); // đợi trên 15ms trước khi VCC lên 4.5V
+   
+    // Tiếp tục khởi tạo theo đúng lưu đồ của nxs 
     LCD_Send4Bit(0b0011);                   //gui 4 bit 0011
     __delay_ms(10);
     LCD_Send4Bit(0b0011);                   //gui 4 bit 0011
@@ -40,10 +41,11 @@ void LCD_Init (void){
     LCD_Send4Bit(0b0010);                   //gui 4 bit 0010
     __delay_ms(10);
     
-    LCD_SendCommand( 0x28 );
-    LCD_SendCommand( 0x0C);
-    LCD_SendCommand( 0x06 );
-    LCD_SendCommand( 0x01 );
+    
+    LCD_SendCommand( 0x28 ); // 0010 1000 function set: 4 bit(D7->D4); hiển thị 2 hàng và kí tự 5x8 điểm ảnh
+    LCD_SendCommand( 0x0C); // 0000 1100 display off:  Hiển thị màn hình, không hiển thị con trỏ và nhấp nháy con trỏ
+    LCD_SendCommand( 0x06 ); // 0000 0110 entry mode set: Tăng địa chỉ bộ đếm hiển thị AC mỗi khi ghi, vị trí con trỏ cũng di chuyển theo
+    LCD_SendCommand( 0x01 ); // 0000 0001 display clear
 }
 
 
@@ -71,9 +73,10 @@ void LCD_Gotoxy(unsigned char row, unsigned char col)
 {
   unsigned char address;    
   if(row==0) 
-      address=0x80 + col;                 //can D7=1
+      address=0x80 + col;                 //D7 bằng 1 để cho phép ghi các giá trị tọa độ của DDRAM vào AC
   else address=0x80 + col+64;             //0x80=1000 0000  
   
-  LCD_SendCommand(address);
+  LCD_SendCommand(address); // gửi lệnh
   __delay_us(50);
+   //khi ở chế độ hiển thị 2 hàng, địa chỉ từ 00h đến 27h cho hàng thứ nhất, và từ 40h đến 67h cho hàng thứ 2.
 }
