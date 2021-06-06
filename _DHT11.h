3c95d79673d07633c57f82963efd18779185b2a9
anhdmh <85148223+anhdmh@users.noreply.github.com>
2021-06-06 17:35:05 +0700

    Update _DHT11.h
    Comment chức năng cho DHT11

--- _DHT11.h
+++ _DHT11.h
@@ -17,17 +17,18 @@ void fan_on();
 
 uint8_t DHT11_start()
 {
-    //----------------------------giao tiep voi DTH11-------------------------
-    Data_Pin_TRIS = 0;
+    Data_Pin_TRIS = 0; // config chân data thành output
     Data_Pin = 0;
-    __delay_ms(30); //doi > 18ms
+    __delay_ms(30); 
+    // cho chân data xuống 0 trên 18ms sau đó kéo lên 1 (20 -> 40us) và thiết lập data thành input
     Data_Pin = 1; 
     Data_Pin_TRIS = 1;
-    //----------------------------kiem tra giao tiep DHT11--------------------
-    __delay_us(40);                     // doi  20-40us thi data xuong 0
+    __delay_us(40);   
+     // lúc này sẽ đợi dht11 phản hồi lại, nếu phản hồi data sẽ xuống 0 còn sau 40us mà không xuống => không giao tiếp được.
     if(Data_Pin == 0)                          
     {
-        __delay_us(80);                 //doi 80us thi Data len 1
+        // data xuống 0 trong khoảng 80us và lên 1 trong khoảng 80us => đã giao tiếp thành công với dht11
+        __delay_us(80);                
         if(Data_Pin == 1)
         {
             __delay_us(80);             
@@ -36,14 +37,20 @@ uint8_t DHT11_start()
     }
 }
 
+/* Sau khi giao tiếp được với DHT11, DHT11 sẽ gửi liên tiếp 40 bit 0 hoặc 1 về MCU, tương ứng chia
+* thành 5 byte kết quả của Nhiệt độ và độ ẩm.
+*/
 uint8_t Read_DTH11()
 {
-    //-----------------nhan du lieu----------------------
+    // sau khi giao tiếp thành công => dht11 sẽ gửi về 5byte dữ liệu và nó sẽ được lưu trong mảng DATA đã khai báo
+    // Vòng for đầu tương ứng với 5 byte dữ liệu
     for(uint8_t i = 0; i<5; i++){
+        // vòng for thứ 2 tương ứng 8 bit dữ liệu
        for(uint8_t ii = 0; ii<8; ii++) {
-        while(Data_Pin == 0);                          //doi len 1
-        __delay_us(50);                         
-        if(Data_Pin == 1)                               //trong >28us ma data van =1 thi se gui bit 1
+        while(Data_Pin == 0);                        
+        __delay_us(50);  
+        // Trong khoảng thời gian lớn hơn 28ms mà datapin vẫn = 1 => bắt đầu gửi các bit
+        if(Data_Pin == 1)                               
         {
             DATA[i] = DATA[i]<<1| 1;
             while(Data_Pin == 1){}
@@ -53,7 +60,7 @@ uint8_t Read_DTH11()
     }
     temp = DATA[2];
     humidity = DATA[0];
-    //---------------------kiem tra data nhan duoc-------------------
+    // checksum
     if(DATA[4] == ( DATA[3]+ DATA[2]+DATA[1]+ DATA[0])) return 1;
 }
 
@@ -70,6 +77,7 @@ void checkTemp(){
     }
 }
 
+// đổi từ số sang chuỗi
 char* getStringData(uint8_t num) {
     char buff[2];
     buff[1] = num % 10 + 48;
